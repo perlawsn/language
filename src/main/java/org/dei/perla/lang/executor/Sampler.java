1aac6cf0d64638358e661dfca50bb7d8fec4c7de
Guido Rota <guido.rota@gmail.com>
2015-04-09 15:27:59 +0000

    Sampler implementation (still work in progress)

--- src/main/java/org/dei/perla/lang/executor/Sampler.java
+++ src/main/java/org/dei/perla/lang/executor/Sampler.java
@@ -3,6 +3,7 @@ package org.dei.perla.lang.executor;
 import org.dei.perla.core.fpc.Fpc;
 import org.dei.perla.core.fpc.Task;
 import org.dei.perla.core.fpc.TaskHandler;
+import org.dei.perla.core.record.Attribute;
 import org.dei.perla.core.record.Record;
 import org.dei.perla.core.utils.Conditions;
 import org.dei.perla.lang.executor.statement.IfEvery;
@@ -11,9 +12,8 @@ import org.dei.perla.lang.executor.statement.Refresh.RefreshType;
 import org.dei.perla.lang.executor.statement.SamplingIfEvery;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author Guido Rota 24/03/15.
@@ -25,37 +25,37 @@ public final class Sampler {
     private static final int SAMPLING = 2;
     private static final int STOPPED = 3;
 
-    private final Fpc fpc;
     private final SamplingIfEvery sampling;
+    private final List<Attribute> atts;
+    private final Fpc fpc;
     private final Refresh refresh;
     private final IfEvery ife;
 
-    private final SamplingHandler sampHandler;
-    private final TaskHandler ifeHandler;
+    private final TaskHandler sampHandler = new SamplingHandler();
+    private final TaskHandler ifeHandler = new IfEveryHandler();
+    private final TaskHandler evtHandler = new EventHandler();
 
     private final AtomicInteger status = new AtomicInteger(INITIALIZING);
     private volatile Duration rate = Duration.ofSeconds(0);
 
     private volatile Task ifeTask = null;
-    private Task samplingTask;
-    private Task eventTask;
+    private Task sampTask;
+    private Task evtTask;
 
-    protected Sampler(SamplingIfEvery sampling, Fpc fpc)
+    protected Sampler(SamplingIfEvery sampling, List<Attribute> atts, Fpc fpc)
             throws IllegalArgumentException {
         Conditions.checkIllegalArgument(sampling.isComplete(),
                 "Sampling clause is not complete.");
         Conditions.checkIllegalArgument(sampling.hasErrors(),
                 "Sampling clause contains errors.");
 
-        this.fpc = fpc;
         this.sampling = sampling;
+        this.atts = atts;
+        this.fpc = fpc;
         this.refresh = sampling.getRefresh();
         this.ife = sampling.getIfEvery();
 
-        sampHandler = new SamplingHandler();
-        ifeHandler = new IfEveryHandler();
-
-        ifeTask = fpc.get(sampling.getIfEveryAttributes(), ifeHandler);
+        ifeTask = fpc.get(sampling.getIfEveryAttributes(), true, ifeHandler);
         if (ifeTask == null) {
             // TODO: improve this
             throw new RuntimeException("cannot sample");
@@ -69,18 +69,20 @@ public final class Sampler {
 
         @Override
         public void complete(Task task) {
-            if (refresh == null  || ifeTask != null ||
+            if (refresh == null || status.intValue() == STOPPED ||
                     refresh.getRefreshType() == RefreshType.EVENT) {
                 return;
             }
 
-            // TODO: check!
-            ifeTask = fpc.get(sampling.getIfEveryAttributes(),
-                    refresh.getDuration(), ifeHandler);
+            ifeTask = fpc.async(sampling.getIfEveryAttributes(), true, ifeHandler);
+            if (ifeTask == null) {
+                // TODO: escalate error
+            }
         }
 
         @Override
         public void newRecord(Task task, Record record) {
+            // TODO: more states are probably needed
             if (status.compareAndSet(SAMPLING, SETTING_RATE)) {
                 Duration d = ife.run(record.values());
                 if (d == rate) {
@@ -89,13 +91,48 @@ public final class Sampler {
                 }
 
                 rate = d;
-                samplingTask.stop();
+                sampTask.stop();
+                // The new sampling rate will be set in the SamplingHandler
+
+            } else if (status.compareAndSet(INITIALIZING, SETTING_RATE)) {
+                Duration d= ife.run(record.values());
+                rate = d;
+                sampTask = fpc.get(atts, false, rate, sampHandler);
+                status.set(SAMPLING);
             }
         }
 
         @Override
         public void error(Task task, Throwable cause) {
-            // TODO: Escalate
+            // TODO: escalate error
+        }
+
+    }
+
+    /**
+     * @author Guido Rota 09/04/2015
+     */
+    private class EventHandler implements TaskHandler {
+
+        @Override
+        public void complete(Task task) {
+            if (status.intValue() != STOPPED) {
+                // TODO: propagate error
+            }
+        }
+
+        @Override
+        public void newRecord(Task task, Record record) {
+            // Single shot sampling when the refresh event is triggered
+            ifeTask = fpc.get(sampling.getIfEveryAttributes(), true, ifeHandler);
+            if (ifeTask == null) {
+                // TODO: Escalate error
+            }
+        }
+
+        @Override
+        public void error(Task task, Throwable cause) {
+            // TODO: escalate error
         }
 
     }
@@ -107,13 +144,18 @@ public final class Sampler {
 
         @Override
         public void complete(Task task) {
-            if (status.intValue() != SETTING_RATE) {
+            if (status.compareAndSet(SETTING_RATE, SAMPLING)) {
+                sampTask = fpc.get(atts, false, rate, sampHandler);
+                status.set(SAMPLING);
                 return;
             }
 
-            samplingTask = fpc.get(sampling.getIfEveryAttributes(), rate,
-                    sampHandler);
-            status.set(SAMPLING);
+            if (status.intValue() == SAMPLING) {
+                // TODO: propagate an error?
+                return;
+            } else if (status.intValue() == STOPPED) {
+                return;
+            }
         }
 
         @Override
