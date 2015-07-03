9a0f6b5b445ed12bf5c8a8b39891ebb6004d0292
Guido Rota <guido.rota@gmail.com>
2015-07-03 10:01:18 +0000

    - Minor changes in perla-language grammar file - Persistence layer scaffolding

--- src/main/java/org/dei/perla/lang/executor/buffer/ArrayBufferReleaser.java
+++ src/main/java/org/dei/perla/lang/executor/buffer/ArrayBufferReleaser.java
@@ -1,7 +1,9 @@
 package org.dei.perla.lang.executor.buffer;
 
 /**
- * The implementation as an abstract class was chosen instead of an
+ * <p> Releases a child view
+ *
+ * <p> The implementation as an abstract class was chosen instead of an
  * implementation as an interface to set the method {@code
  * releaseChildView} visibility to {@code protected}.
  *
---
+++ src/main/java/org/dei/perla/lang/persistence/StreamRegistry.java
@@ -0,0 +1,7 @@
+package org.dei.perla.lang.persistence;
+
+/**
+ * @author Guido Rota 03/07/15.
+ */
+public interface StreamRegistry {
+}
