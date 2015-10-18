8f2aa9d83b81528b4c080b67b2d7783120a6f5e2
Guido Rota <guido.rota@gmail.com>
2015-10-18 13:06:50 +1100

    ExecutionCondition test

--- src/test/java/org/dei/perla/lang/query/statement/ExecutionConditionsTest.java
+++ src/test/java/org/dei/perla/lang/query/statement/ExecutionConditionsTest.java
@@ -2,12 +2,13 @@ package org.dei.perla.lang.query.statement;
 
 import org.dei.perla.core.fpc.Attribute;
 import org.dei.perla.core.fpc.DataType;
-import org.dei.perla.core.utils.Errors;
 import org.dei.perla.lang.query.expression.Constant;
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
@@ -19,28 +20,34 @@ import static org.junit.Assert.assertTrue;
  */
 public class ExecutionConditionsTest {
 
-    private static final List<Attribute> specs =
-            Arrays.asList(new Attribute[] {
-            Attribute.create("temperature", DataType.ANY),
-            Attribute.create("pressure", DataType.INTEGER)
-    });
+    private static final Attribute intAtt =
+            Attribute.create("integer", DataType.INTEGER);
+    private static final Attribute floatAtt =
+            Attribute.create("float", DataType.FLOAT);
 
     @Test
     public void testSimple() {
-        throw new RuntimeException("unimplemented");
-//        Errors err = new Errors();
-//
-//        ExecutionConditions ec = ExecutionConditions.create(Constant.TRUE,
-//                specs, Refresh.NEVER, err);
-//        assertThat(ec, notNullValue());
-//        assertTrue(err.isEmpty());
-//        assertThat(ec.getCondition(), equalTo(Constant.TRUE));
-//        assertThat(ec.getRefresh(), equalTo(Refresh.NEVER));
-//        List<Attribute> s = ec.getSpecifications();
-//        assertThat(s.size(), equalTo(specs.size()));
-//        assertTrue(s.contains(specs.get(0)));
-//        assertTrue(s.contains(specs.get(1)));
-//        assertTrue(ec.getAttributes().isEmpty());
+        Set<Attribute> specs = new HashSet<>();
+        specs.add(intAtt);
+        specs.add(floatAtt);
+
+        List<Attribute> atts = new ArrayList<>();
+        atts.add(intAtt);
+        atts.add(floatAtt);
+
+        ExecutionConditions ec = new ExecutionConditions(specs, Constant.TRUE,
+                atts, Refresh.NEVER);
+        assertThat(ec, notNullValue());
+        assertThat(ec.getCondition(), equalTo(Constant.TRUE));
+        assertThat(ec.getRefresh(), equalTo(Refresh.NEVER));
+
+        Set<Attribute> specAtts = ec.getSpecifications();
+        assertThat(specAtts.size(), equalTo(specs.size()));
+        assertTrue(specAtts.containsAll(specs));
+
+        List<Attribute> condAtts = ec.getAttributes();
+        assertThat(condAtts.size(), equalTo(atts.size()));
+        assertTrue(condAtts.containsAll(atts));
     }
 
 }
