8585c04907ecb9fa41e8ad638aa84d969fb705ac
Guido Rota <guido.rota@gmail.com>
2015-10-26 23:21:26 +1100

    SelectionExecutor implementation

--- src/main/java/org/dei/perla/lang/executor/statement/SelectionExecutor.java
+++ src/main/java/org/dei/perla/lang/executor/statement/SelectionExecutor.java
@@ -4,13 +4,18 @@ import org.dei.perla.core.fpc.Attribute;
 import org.dei.perla.core.fpc.Fpc;
 import org.dei.perla.lang.executor.buffer.ArrayBuffer;
 import org.dei.perla.lang.executor.buffer.Buffer;
+import org.dei.perla.lang.executor.buffer.BufferView;
+import org.dei.perla.lang.executor.buffer.UnreleasedViewException;
 import org.dei.perla.lang.query.expression.Expression;
 import org.dei.perla.lang.query.expression.LogicValue;
 import org.dei.perla.lang.query.statement.Sampling;
 import org.dei.perla.lang.query.statement.SelectionStatement;
+import org.dei.perla.lang.query.statement.WindowSize;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Executor for the {@link SelectionStatement} class
@@ -19,16 +24,29 @@ import java.util.concurrent.atomic.AtomicBoolean;
  */
 public final class SelectionExecutor {
 
+    private static final int READY = 0;
+    private static final int RUNNING = 1;
+    private static final int STOPPED = 2;
+
+    private static final ScheduledExecutorService timer =
+            Executors.newScheduledThreadPool(5);
+    private static final ExecutorService exec =
+            Executors.newCachedThreadPool();
+
     private final Fpc fpc;
     private final SelectionStatement query;
     private final List<Attribute> selAtts;
     private final Expression where;
+    private final WindowSize every;
     private final QueryHandler<? super SelectionStatement, Object[]> handler;
 
-    private final AtomicBoolean running = new AtomicBoolean(false);
+    private final Lock lk = new ReentrantLock();
+    private int status = READY;
 
     private final Buffer buffer;
     private final SamplerManager sampMgr;
+    private int everyCount;
+    private ScheduledFuture<?> everyThread;
 
     public SelectionExecutor(
             SelectionStatement query,
@@ -38,6 +56,7 @@ public final class SelectionExecutor {
         this.query = query;
         selAtts = query.getAttributes();
         where = query.getWhere();
+        every = query.getEvery();
         this.handler = handler;
         buffer = new ArrayBuffer(selAtts);
         sampMgr = new SamplerManager(
@@ -47,21 +66,120 @@ public final class SelectionExecutor {
     }
 
     public void start() {
-        if (!running.compareAndSet(false, true)) {
-            return;
+        lk.lock();
+        try {
+            if (status == RUNNING) {
+                return;
+            } else if (status == STOPPED) {
+                throw new IllegalStateException(
+                        "Cannot restart SelectionExecutor");
+            }
+            status = RUNNING;
+            startEvery();
+            sampMgr.start();
+        } finally {
+            lk.unlock();
         }
-        sampMgr.start();
+    }
+
+    private void startEvery() {
+        switch (every.getType()) {
+            case TIME:
+                long ms = every.getDuration().toMillis();
+                everyThread = timer.scheduleAtFixedRate(
+                        new EveryRunner(),
+                        ms,
+                        ms,
+                        TimeUnit.MILLISECONDS);
+                break;
+            case SAMPLE:
+                everyCount = every.getSamples();
+                break;
+            default:
+                throw new RuntimeException(
+                        "Unknown window size type " + every.getType()
+                );
+        }
     }
 
     public void stop() {
-        if (!running.compareAndSet(true, false)) {
-            return;
+        lk.lock();
+        try {
+            if (status == STOPPED) {
+                return;
+            } else if (status == READY) {
+                throw new IllegalStateException(
+                        "SelectionExecutor has not been started");
+            }
+            if (everyThread != null) {
+                everyThread.cancel(true);
+                everyThread = null;
+            }
+            sampMgr.stop();
+        } finally {
+            lk.unlock();
         }
-        sampMgr.stop();
     }
 
     public boolean isRunning() {
-        return running.get();
+        lk.lock();
+        try {
+            return status == RUNNING;
+        } finally {
+            lk.unlock();
+        }
+    }
+
+
+    /**
+     * Every runner
+     */
+    private final class EveryRunner implements Runnable {
+
+        public void run() {
+            lk.lock();
+            try {
+                if (status != RUNNING) {
+                    return;
+                }
+                BufferView view = buffer.createView();
+                exec.execute(new SelectionRunner(view));
+            } catch (UnreleasedViewException e) {
+                handler.error(query, e);
+                stop();
+            } finally {
+                lk.unlock();
+            }
+        }
+
+    }
+
+
+    /**
+     * Selection runner
+     */
+    private final class SelectionRunner implements Runnable {
+
+        private final BufferView view;
+
+        public SelectionRunner(BufferView view) {
+            this.view = view;
+        }
+
+        public void run() {
+            List<Object[]> res = query.select(view);
+            view.release();
+            lk.lock();
+            try {
+                if (status != RUNNING) {
+                    return;
+                }
+                res.forEach((r) -> handler.data(query, r));
+            } finally {
+                lk.unlock();
+            }
+        }
+
     }
 
 
@@ -83,7 +201,31 @@ public final class SelectionExecutor {
                 return;
             }
 
-            buffer.add(sample);
+            lk.lock();
+            try {
+                buffer.add(sample);
+                if (every.getType() == WindowSize.WindowType.SAMPLE) {
+                    triggerCountSampling();
+                }
+            } finally {
+                lk.unlock();
+            }
+        }
+
+        private void triggerCountSampling() {
+            everyCount--;
+            if (everyCount != 0) {
+                return;
+            }
+
+            try {
+                everyCount = every.getSamples();
+                BufferView view = buffer.createView();
+                exec.execute(new SelectionRunner(view));
+            } catch(UnreleasedViewException e) {
+                handler.error(query, e);
+                stop();
+            }
         }
 
     }
