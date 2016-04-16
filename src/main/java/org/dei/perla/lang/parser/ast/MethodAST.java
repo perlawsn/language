65a48dcb663ac6187c6674fba59a869e8991c89b
racheleSabatino <rachele_1991@libero.it>
2016-04-16 20:04:55 +0200

    aggiunta classe utility

--- src/main/java/org/dei/perla/lang/parser/ast/MethodAST.java
+++ src/main/java/org/dei/perla/lang/parser/ast/MethodAST.java
@@ -12,6 +12,8 @@ import org.dei.perla.lang.parser.TypeVariable;
 import org.dei.perla.lang.parser.ast.ExpressionAST;
 import org.dei.perla.lang.query.expression.Expression;
 import org.dei.perla.lang.query.expression.MethodExp;
+import org.dei.perla.lang.utility.Utility;
+
 
 /*
  * A function requested by the user
@@ -19,55 +21,81 @@ import org.dei.perla.lang.query.expression.MethodExp;
  */
 public class MethodAST extends ExpressionAST{
 
-	private String className;
 	private String methodName;
 	private DataType type;
 	private boolean inferenced = false;
 	
-	private MethodAST(Token token, String className, String methodName, DataType type) {
+	private MethodAST(Token token, String methodName, DataType type) {
 		super(token);
-		this.className = className;
 		this.methodName = methodName;
 		this.type = type;
 	}
 	
 	private MethodAST(String className, String methodName) {
 		super(null);
-		this.className = className;
 		this.methodName = methodName;
 	}
 	
-	public static MethodAST create(Token token, String methodName, ParserContext ctx){
-		int index = methodName.lastIndexOf( '.' );
-		int len = methodName.length();
-		String className = methodName.substring(0,index);
-		String function = methodName.substring(index+1, len);
-		Class classe = Class.class;
-		try {
-			classe = Class.forName(className);
-		} catch (ClassNotFoundException e) {
-			ctx.addError(e.getMessage());
-			className = "";
-		}
-		Method getFunctionName = null;
+//	public static MethodAST create(Token token, String methodName, ParserContext ctx){
+//		int index = methodName.lastIndexOf( '.' );
+//		int len = methodName.length();
+//		String className = methodName.substring(0,index);
+//		String function = methodName.substring(index+1, len);
+//		Class classe = Class.class;
+//		try {
+//			classe = Class.forName(className);
+//		} catch (ClassNotFoundException e) {
+//			ctx.addError(e.getMessage());
+//			className = "";
+//		}
+//		Method getFunctionName = null;
+//		DataType type = null;
+//		try {
+//			getFunctionName = classe.getDeclaredMethod(function, null);
+//			Object result = getFunctionName.invoke(classe.newInstance());
+//			String classResult = result.getClass().getCanonicalName();
+//			index = classResult.lastIndexOf( '.' );
+//			len = classResult.length();
+//			String typeString = classResult.substring(index+1, len);
+//			type = ConcreteType.parse(typeString);
+//			if(type == null) 
+//				ctx.addError("The function returns an unknown type. The available types are: "
+//						+ "String, Integer, Boolean, Id, Float, TimeStamp");
+//		} catch (NoSuchMethodException | SecurityException | IllegalAccessException |
+//				IllegalArgumentException | InvocationTargetException | InstantiationException e) {
+//			ctx.addError(e.getMessage());
+//		}
+//		return new MethodAST(token, className, function, type);
+//	}
+	
+	public static MethodAST create(Token token, String function, ParserContext ctx){
 		DataType type = null;
-		try {
-			getFunctionName = classe.getDeclaredMethod(function, null);
-			Object result = getFunctionName.invoke(classe.newInstance());
-			String classResult = result.getClass().getCanonicalName();
-			index = classResult.lastIndexOf( '.' );
-			len = classResult.length();
-			String typeString = classResult.substring(index+1, len);
-			type = ConcreteType.parse(typeString);
-			if(type == null) 
-				ctx.addError("The function returns an unknown type. The available types are: "
-						+ "String, Integer, Boolean, Id, Float, TimeStamp");
-		} catch (NoSuchMethodException | SecurityException | IllegalAccessException |
-				IllegalArgumentException | InvocationTargetException | InstantiationException e) {
-			ctx.addError(e.getMessage());
+		if(function.contains("()")){
+			int len = function.length()-2;
+			function = function.substring(0, len);
 		}
-		return new MethodAST(token, className, function, type);
+		Utility u = Utility.getInstance();
+		if(!u.existsFunction(function)){
+			ctx.addError("The function " + function + " does not exist");
+		}
+		else {
+			Object result = u.retrieveValueFunction(function, null);
+			if (result == null) 
+				ctx.addError("The function " + function + " returned a null result");
+			else {
+				String classResult = result.getClass().getCanonicalName();
+				int index = classResult.lastIndexOf( '.' );
+				int len = classResult.length();
+				String typeString = classResult.substring(index+1, len);
+				type = ConcreteType.parse(typeString);
+				if(type == null) 
+					ctx.addError("The function returns an unknown type. The available types are: "
+							+ "String, Integer, Boolean, Id, Float, TimeStamp");
+			}		
+		}
+		return new MethodAST(token, function, type);
 	}
+	
 	
 	public static MethodAST create(String methodName, ParserContext ctx){
 		return MethodAST.create(null, methodName, ctx);
@@ -90,7 +118,7 @@ public class MethodAST extends ExpressionAST{
         inferenced = true;
         boolean res = true;
         if (!bound.restrict(type)) {
-            String msg = "Incompatible type for return value of function '" + className + "." + methodName ;
+            String msg = "Incompatible type for return value of function '" + methodName ;
             ctx.addError(msg);
             res = false;
         }
@@ -100,7 +128,7 @@ public class MethodAST extends ExpressionAST{
 
 	@Override
 	protected Expression toExpression(ParserContext ctx, AttributeOrder ord) {
-		return MethodExp.create(className, methodName, type); 
+		return MethodExp.create(methodName, type); 
 	}
 
 
--- src/main/java/org/dei/perla/lang/query/expression/MethodExp.java
+++ src/main/java/org/dei/perla/lang/query/expression/MethodExp.java
@@ -1,21 +1,17 @@
 package org.dei.perla.lang.query.expression;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 import org.dei.perla.core.fpc.DataType;
 import org.dei.perla.lang.executor.buffer.BufferView;
 import org.dei.perla.lang.query.expression.Expression;
+import org.dei.perla.lang.utility.Utility;
 
 public class MethodExp extends Expression {
 	
 	private final String methodName;
-	private final String className; 
 	private final DataType type;
 	
-	private MethodExp(String methodName, String className, DataType type) {
+	private MethodExp(String methodName, DataType type) {
 		this.methodName = methodName;
-		this.className = className;
 		this.type = type;
 	}
 
@@ -27,24 +23,16 @@ public class MethodExp extends Expression {
 	
 	@Override
 	public Object run(Object[] sample, BufferView buffer) {
-		Class classe = Class.class;
-		try {
-			classe = Class.forName(className);
-		} catch (ClassNotFoundException e) {
-			return null;
+		Object result = null;
+		Utility u = Utility.getInstance();
+		if(!u.existsFunction(methodName)){
+			System.out.println("The function " + methodName + " does not exist ");
+			return result;
 		}
-		Method getFunctionName;
-		try {
-			getFunctionName = classe.getDeclaredMethod(methodName, null);
-		} catch (NoSuchMethodException | SecurityException e) {
-			return null;
-		}
-		Object result;
-		try {
-			result = getFunctionName.invoke(classe.newInstance());
-		} catch (IllegalAccessException | IllegalArgumentException
-				| InvocationTargetException | InstantiationException e) {
-			result = null;
+		else {
+			result = u.retrieveValueFunction(methodName, null);
+			if (result == null) 
+				System.out.println("The function " + methodName + " returned a null result");		
 		}
 		return result;
 	}
@@ -52,15 +40,13 @@ public class MethodExp extends Expression {
 	@Override
     protected void buildString(StringBuilder bld) {
         bld.append("(")
-                .append(className)
-                .append(".")
                 .append(methodName)
                 .append(")");
     }
 
 	
-	public static MethodExp create(String className, String methodName, DataType type){
-		return new MethodExp(className, methodName, type);
+	public static MethodExp create(String methodName, DataType type){
+		return new MethodExp(methodName, type);
 	}
 
 }
